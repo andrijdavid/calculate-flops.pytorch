5969e0bab618d061f5cce27709b932cafa3162da
Andrij David <andrijdavid@gmail.com>
2025-08-19 17:36:36 +0200

    Bump

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="calflops2",
-    version="0.1.0",
+    version="0.1.1",
     author="Andrij David",
     author_email="david@andrij.me",
     description="A tool to calculate FLOPs and Params for neural networks",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/andrijdavid/calculate-flops.pytorch",
-    packages=setuptools.find_packages(),
+    packages=["calflops"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
